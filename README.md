c095c4133745d1b793c6f44504c9d343a7fd6edb
Gavin Reynolds <gavin@chef.io>
2020-11-14 10:35:59 +0000

    Improve examples
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Bulk conversion of Role and Environment file `default_attributes` and `override_
 chef exec ruby jsontorb.rb roles/example1.json | tee -a test-policy.rb
 chef exec ruby jsontorb.rb environments/dev.json  | tee -a test-policy.rb
 chef exec ruby jsontorb.rb environments/prod.json  | tee -a test-policy.rb
+chef exec cookstyle -a test-policy.rb
 ```
 
 For environment files, attributes are prefixed with the environment name for Policyfile Attribute Hoisting based on Policy Group.
--- test-policy.rb
+++ test-policy.rb
@@ -13,52 +13,41 @@ default_source :supermarket
 run_list 'chef-client::default', 'audit::default'
 
 # default attributes from example1 role
-default['corp']['packages'] = ["httpd"]
-default['apache']['something']['release'] = '23.el7'
-default['2']['test'] = 'test'
-default['topleveltest'] = 'test'
-
-# default attributes from example1 role
-default['corp']['packages'] = ["httpd"]
+default['corp']['packages'] = ['httpd']
 default['apache']['something']['release'] = '23.el7'
 default['2']['test'] = 'test'
 default['topleveltest'] = 'test'
 
 # default attributes from dev environment
-default['dev']['corp']['packages'] = ["httpd"]
+default['dev']['corp']['packages'] = ['httpd']
+
+# default attributes from example1 role
+default['corp']['packages'] = ['httpd']
+default['apache']['something']['release'] = '23.el7'
+default['2']['test'] = 'test'
+default['topleveltest'] = 'test'
+
+# default attributes from dev environment
+default['dev']['corp']['packages'] = ['httpd']
 
 # default attributes from prod environment
-default['prod']['corp']['packages'] = ["httpd", "somethingelse"]
+default['prod']['corp']['packages'] = %w(httpd somethingelse)
 default['prod']['apache']['something']['release'] = '23.el7'
 default['prod']['2']['test'] = 'test'
 default['prod']['topleveltest'] = 'test'
 
 # override attributes from prod environment
-override['prod']['a']['ABC']['env_name'] = 'DEV'
-override['prod']['a']['ABC']['database']['server'] = 'a'
-override['prod']['a']['ABC']['database']['name'] = 'qa'
-override['prod']['a']['ABC']['database']['user'] = 'qa'
-override['prod']['a']['ABC']['database']['sid'] = '123'
-override['prod']['a']['ABC']['database']['port'] = '1000'
-override['prod']['a']['webserver']['conn']['pool_size'] = '10'
-override['prod']['a']['webserver']['something']['somethingmore']['test'] = [1000, 2000]
-override['prod']['a']['server']['uri'] = 'example.com'
-override['prod']['a']['s3']['remote_path'] = '/path/'
-override['prod']['a']['tar.gz_file'] = '123.tar.gz'
-override['prod']['a']['email']['onError']['business'] = '123@example.com'
-override['prod']['a']['email']['onError']['nonbusiness'] = '123@examle.com'
-override['prod']['a']['taskscheduler']['instance'] = ["123"]
-override['prod']['b']['ABC']['env_name'] = 'DEV'
-override['prod']['b']['ABC']['database']['server'] = 'a'
-override['prod']['b']['ABC']['database']['name'] = 'qa'
-override['prod']['b']['ABC']['database']['user'] = 'qa'
-override['prod']['b']['ABC']['database']['sid'] = '123'
-override['prod']['b']['ABC']['database']['port'] = '1000'
-override['prod']['b']['webserver']['conn']['pool_size'] = '10'
-override['prod']['b']['webserver']['something']['somethingmore']['test'] = [1000, 2000]
-override['prod']['b']['server']['uri'] = 'example.com'
-override['prod']['b']['s3']['remote_path'] = '/path/'
-override['prod']['b']['tar.gz_file'] = '123.tar.gz'
-override['prod']['b']['email']['onError']['business'] = '123@example.com'
-override['prod']['b']['email']['onError']['nonbusiness'] = '123@examle.com'
-override['prod']['b']['taskscheduler']['instance'] = ["123"]
+override['prod']['ABC']['env_name'] = 'DEV'
+override['prod']['ABC']['database']['server'] = 'a'
+override['prod']['ABC']['database']['name'] = 'qa'
+override['prod']['ABC']['database']['user'] = 'qa'
+override['prod']['ABC']['database']['sid'] = '123'
+override['prod']['ABC']['database']['port'] = '1000'
+override['prod']['webserver']['conn']['pool_size'] = '10'
+override['prod']['webserver']['something']['somethingmore']['test'] = [1000, 2000]
+override['prod']['server']['uri'] = 'example.com'
+override['prod']['s3']['remote_path'] = '/path/'
+override['prod']['tar.gz_file'] = '123.tar.gz'
+override['prod']['email']['onError']['business'] = '123@example.com'
+override['prod']['email']['onError']['nonbusiness'] = '123@examle.com'
+override['prod']['taskscheduler']['instance'] = ['123']
